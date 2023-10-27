3790a454211f4b88ef385f501ce9c13693f9e9f5
AbhishekSingh971 <abhitil654@gmail.com>
2023-10-27 22:51:44 +0530

    updated

--- server.js
+++ server.js
@@ -19,9 +19,9 @@ const app = express();
 
 //middelwares
 var corsOptions = {
-    origin: 'https://ecommerce-frontend-five-omega.vercel.app/',
+    origin: 'https://ecommerce-frontend-five-omega.vercel.app',
     optionsSuccessStatus: 200,
-    AccessControlAllowOrigin: "https://ecommerce-frontend-five-omega.vercel.app/"
+    AccessControlAllowOrigin: "https://ecommerce-frontend-five-omega.vercel.app"
 // some legacy browsers (IE11, various SmartTVs) choke on 204
   }
 app.use(express.json());
