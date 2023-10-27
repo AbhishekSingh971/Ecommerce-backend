068f0b8fe1a7c1b0d3c2bcfbeec135436400bab0
AbhishekSingh971 <abhitil654@gmail.com>
2023-10-27 22:25:23 +0530

    update

--- server.js
+++ server.js
@@ -20,7 +20,9 @@ const app = express();
 //middelwares
 var corsOptions = {
     origin: 'http://localhost:3000',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+    optionsSuccessStatus: 200,
+    AccessControlAllowOrigin: "http://localhost:3000"
+// some legacy browsers (IE11, various SmartTVs) choke on 204
   }
 app.use(express.json());
 app.use(morgan('dev'));
